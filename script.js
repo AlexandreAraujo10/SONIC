19f79ae7b1bc949ac7986ee30ef18e1d3826c8c8
AlexandreAraujo10 <xnd.araujo@gmail.com>
2023-11-22 13:07:16 -0300

    primeiros comandos

---
+++ script.js
@@ -0,0 +1,8 @@
+const sonic = document.querySelector(".sonic");
+
+const jump = () => {
+    sonic.classList.add("jump");
+};
+
+
+document.addEventListener("click", jump); /* quando apertar click, executará essa função(jump) */
